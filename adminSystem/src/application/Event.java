8fb5a4933bfdc3ce238db106a6aec10b1e6bd868
Luke Mason <L.Mason6@uni.brighton.ac.uk>
2018-04-23 00:19:49 +0100

    GUI & Model Improvements
    New GUI works, added JFX Libaries, added more data values to Event model class.

--- adminSystem/src/application/Event.java
+++ adminSystem/src/application/Event.java
@@ -1,32 +1,58 @@
 package application;
 
+import java.util.Date;
+
 public class Event {
 
+	private String eventID;
 	private String eventName;
-	private String eventAddress;
+	private String eventInfo;
+	private String eventAddressLineOne;
+	private String eventAdderssLineTwo;
 	private String eventPostcode;
 	private String eventTel;
-	private String eventInfo;
+	private String eventTC;
+	private Date eventStartDate;
+	private Date eventEndDate;
 	
-	public Event(String eventName, String eventAddress, String eventPostcode, String eventTel, String eventInfo) {
+	public Event(String eventID, String eventName, String eventInfo, String eventAddressLineOne, String eventAddressLineTwo,
+			String eventPostcode, String eventTel, String eventTC, Date eventStartDate, Date eventEndDate){
+		this.eventID = eventID;
 		this.eventName = eventName;
-		this.eventAddress = eventAddress;
+		this.eventInfo = eventInfo;
+		this.eventAddressLineOne = eventAddressLineOne;
+		this.eventAdderssLineTwo = eventAddressLineTwo;
 		this.eventPostcode = eventPostcode;
 		this.eventTel = eventTel;
-		this.eventInfo = eventInfo;
+		this.eventTC = eventTC;
+		this.eventStartDate = eventStartDate;
+		this.eventEndDate = eventEndDate;
 	}
 	
-	// getters
-	public String getEventName() { return eventName; }
-	public String getEventAddress() { return eventAddress; }
-	public String getEventPostcode() { return eventPostcode; }
-	public String getEventTel() { return eventTel; }
-	public String getEventInfo() { return eventInfo; }
+	//getters
+	public String getEventID(){ return eventID; }
+	public String getEventInfo(){ return eventInfo; }
+	public String getEventAddressLineOne(){ return eventAddressLineOne; }
+	public String getEventAdderssLineTwo(){ return eventAdderssLineTwo; }
+	public String getEventPostcode(){ return eventPostcode; }
+	public String getEventTel(){ return eventTel; }
+	public String getEventTC(){ return eventTC; }
+	public Date getEventStartDate(){ return eventStartDate; }
+	public Date getEventEndDate(){ return eventEndDate; }
 	
 	//setters
-	public void setEventName(String eventName) { this.eventName = eventName; }
-	public void setEventAddress(String eventAddress) { this.eventAddress = eventAddress; }
-	public void setEventPostcode(String eventPostcode) { this.eventPostcode = eventPostcode; }
-	public void setEventTel(String eventTel) { this.eventTel = eventTel; }
-	public void setEventInfo(String eventInfo) { this.eventInfo = eventInfo; }
+	public void setEventID(String eventID){ this.eventID = eventID; }
+	public void setEventName(String eventName){ this.eventName = eventName; }
+	public void setEventInfo(String eventInfo){ this.eventInfo = eventInfo; }
+	public void setEventAddressLineOne(String eventAddressLineOne){ this.eventAddressLineOne = eventAddressLineOne; }
+	public void setEventAdderssLineTwo(String eventAdderssLineTwo){ this.eventAdderssLineTwo = eventAdderssLineTwo; }
+	public void setEventPostcode(String eventPostcode){ this.eventPostcode = eventPostcode; }
+	public void setEventTel(String eventTel){ this.eventTel = eventTel; }
+	public void setEventTC(String eventTC){ this.eventTC = eventTC; }
+	public void setEventStartDate(Date eventStartDate){ this.eventStartDate = eventStartDate; }
+	public void setEventEndDate(Date eventEndDate){ this.eventEndDate = eventEndDate; }
+	
+	
+	
+	
 }
